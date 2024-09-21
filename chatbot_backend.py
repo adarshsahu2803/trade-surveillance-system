36c5288a61bb01e6f558bdcddaf34f2f441d651a
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-09-21 14:51:45 +0000

    fix: hosting

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -146,3 +146,5 @@ def query():
 
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
