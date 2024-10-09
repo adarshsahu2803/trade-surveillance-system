a124b9af232b689b939a6ef1aacfa7aae817afb8
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-10-09 06:06:25 +0000

    News summary

--- pages/News.py
+++ pages/News.py
@@ -7,6 +7,9 @@ from datetime import datetime
 import plotly.express as px  
 from dotenv import load_dotenv
 import os
+import yfinance as yf
+import matplotlib.pyplot as plt
+import numpy as np
 
 access_key = os.getenv('ACCESS_KEY_ID')
 secret_access_key = os.getenv('SECRET_ACCESS_KEY')
@@ -26,25 +29,25 @@ def show_news():
     st.dataframe(df_fin, hide_index=True)
 
     if not df_fin.empty:
-        product_key = df_fin['ProductKey'].iloc[0] 
+        product_key = df_fin['ProductKey'].iloc[0]
         product_key = product_key[:3] + '/' + product_key[3:]
         TRADE_ENTRY = product_key
     else:
         TRADE_ENTRY = "stocks OR markets OR finance OR trading"
 
     # Hardcoded trade entry and date range
-    HARD_CODED_FROM_DATE = "2024-08-24"
-    HARD_CODED_TO_DATE = "2024-09-22"
+    HARD_CODED_FROM_DATE = "2024-09-09"
+    HARD_CODED_TO_DATE = "2024-08-05"
 
     # Initialize the Bedrock runtime client
     bedrock_runtime = boto3.client(service_name='bedrock-runtime', aws_access_key_id=access_key,aws_secret_access_key=secret_access_key, region_name=region_name)
-    
+   
 
     #Convert currency pair to natural language
     def convert_currency_pair_for_api(currency_pair):
     # Dictionary to map currency codes to full names without spaces
         currency_map = {
-            'EUR': 'Euro',
+            'EUR': 'Euro%20Trade',
             'JPY': 'Yen',
             'USD': 'US%20Dollar',
             'GBP': 'British%20Pound',
@@ -58,15 +61,15 @@ def show_news():
             'INR': 'Rupee',
             # Add more currency codes as needed
         }
-        
+       
         # Extract the two currency codes
         first_currency = currency_pair[:3]
         second_currency = currency_pair[3:]
-        
+       
         # Convert codes to full names without spaces using the dictionary
         first_currency_full = currency_map.get(first_currency, first_currency)
         second_currency_full = currency_map.get(second_currency, second_currency)
-        
+       
         return first_currency_full, second_currency_full
     # Function to fetch articles
     def get_trade_articles(trade_entry, from_date, to_date):
@@ -79,7 +82,7 @@ def show_news():
         open_page = res.json()
         articles = open_page.get("articles", [])
         return articles[:3]
-    
+   
     # Function to fetch additional articles on "trading"
     def get_related_articles(from_date, to_date, trade_entry):
         currency1, currency2 = convert_currency_pair_for_api(trade_entry)
@@ -93,13 +96,13 @@ def show_news():
         open_page = res.json()
         articles = open_page.get("articles", [])
         if not articles:
-            main_url = f"https://newsapi.org/v2/everything?q={currency1}&from={from_date}&to={to_date}"
+            main_url = f"https://newsapi.org/v2/everything?q={currency2}&from={from_date}&to={to_date}"
             res = requests.get(main_url, params=query_params)
             open_page = res.json()
             articles = open_page.get("articles", [])
 
         return articles
-    
+   
     # Function to generate summaries
     def generate_summary(article_content):
         prompt = (
@@ -125,49 +128,91 @@ def show_news():
         response_body = json.loads(response['body'].read())
         generated_text = response_body['content'][0]['text']
         return generated_text
+       
+    def total_summary(article_content):
+        prompt = (
+            "Please summarize the following multiple articles in a concise manner  within 5 lines, "
+            "highlighting the key points and main ideas. "
+            "Avoid using phrases like 'based on the information I have' or 'I don't have sufficient Information.' even if there is limited information or the news appears truncated. "
+            "Here is the article content:\n\n"
+            f"{article_content}\n\n"
+            "Summary:"
+        )
+        request_body = json.dumps({
+            "anthropic_version": "bedrock-2023-05-31",
+            "max_tokens": 1000,
+            "temperature": 0.7,
+            "messages": [
+                {"role": "user", "content": prompt}
+            ]
+        })
+        response = bedrock_runtime.invoke_model(
+            modelId='anthropic.claude-3-5-sonnet-20240620-v1:0',
+            body=request_body
+        )
+        response_body = json.loads(response['body'].read())
+        generated_text = response_body['content'][0]['text']
+        return generated_text
 
     leftPane, emptyPane, rightPane = st.columns([9, 0.5, 5])
-    
+   
+    article_text = ''
+   
     with leftPane:
+       
         # Use hardcoded dates
         from_date_str = HARD_CODED_FROM_DATE
         to_date_str = HARD_CODED_TO_DATE
-        
+       
         # Load articles related to the hardcoded trade entry and selected dates
         articles = get_trade_articles(TRADE_ENTRY, from_date_str, to_date_str)
 
-        # Prepare data for the graph (number of articles vs. time)
-        if articles:
-            df = pd.DataFrame(articles)
-            df['publishedAt'] = pd.to_datetime(df['publishedAt']).dt.date  # Convert publishedAt to date only
-            # Create a date range from from_date to to_date
-            full_date_range = pd.date_range(start=from_date_str, end=to_date_str).date
-            df_full_dates = pd.DataFrame(full_date_range, columns=['publishedAt'])
-        
-            # Group articles by published date and count
-            articles_by_date = df.groupby('publishedAt').size().reset_index(name='Article Count')
-        
-            # Merge with the full date range to fill missing dates with zero counts
-            articles_by_date_full = pd.merge(df_full_dates, articles_by_date, on='publishedAt', how='left')
-            articles_by_date_full['Article Count'].fillna(0, inplace=True)
-        
-            # Plot line graph with full date range
-            st.subheader(f"Trade News Summaries for {product_key}")
-            fig = px.line(articles_by_date_full, x='publishedAt', y='Article Count', title=f"Article Count for {product_key} Over Time")
-            fig.update_xaxes(title_text="Published On")
-            st.plotly_chart(fig)
-        else:
-            # Create an empty DataFrame for the date range
-            full_date_range = pd.date_range(start=from_date_str, end=to_date_str).date
-            df_full_dates = pd.DataFrame(full_date_range, columns=['publishedAt'])
-            df_full_dates['Article Count'] = 0  # Set Article Count to 0
+        # Define currency pair to yfinance ticker map
+        currency_pair_map = {
+            'USDHKD': 'HKD=X',
+            'USDKRW': 'KRW=X',
+            'EURJPY': 'EURJPY=X'
+        }
+        product_key_slash = ""
+        if len(product_key) == 7:
+            product_key_slash = product_key[:3] + product_key[4:]
+        # Get the correct yfinance ticker
+        yf_ticker = currency_pair_map[product_key_slash]
+        # alert_creation_date = df_fin['AlertDate'].iloc[0]
+        # end_date = df_fin['AlertCreationDate'].iloc[0]
+        alert_creation_date = "2024-09-11"
+        end_date = "2024-09-12"
 
-            st.subheader(f"Trade News Summaries for {product_key}")
-            # Plot an empty graph with zeros
-            fig = px.line(df_full_dates, x='publishedAt', y='Article Count', title=f"Article Count for {product_key} Over Time (No Articles)")
-            fig.update_xaxes(title_text="Published On")
-            st.plotly_chart(fig)
-    
+        # Download hourly data
+        data = yf.download(yf_ticker, start=alert_creation_date, end=end_date, interval='1h')
+
+        # Check if data is not empty
+        if not data.empty:
+            np.random.seed(42)  # For reproducibility
+            hourly_volume = np.random.randint(1000, 10000, size=len(data))
+           
+            # Add the synthetic volume to the data
+            data['Volume'] = hourly_volume
+
+            # Plot Close Price
+            fig1 = px.line(data, x=data.index, y='Close', title=f'{product_key_slash} Hourly Price (Close)',
+                        labels={'Close': 'Price', 'index': 'Hour'})
+           
+            fig1.update_layout(xaxis_title='Time', yaxis_title='Price', template='plotly_dark',
+                            title_x=0.25, margin=dict(t=80, l=40, r=40, b=40))
+           
+            # Plot Volume
+            fig2 = px.bar(data, x=data.index, y='Volume', title=f'{product_key_slash} Hourly Trade Volume',
+                        labels={'Volume': 'Volume', 'index': 'Hour'})
+            fig2.update_layout(xaxis_title='Time', yaxis_title='Volume', template='plotly_dark',
+                            title_x=0.25, margin=dict(t=80, l=40, r=40, b=40))
+
+            # Display the Price & volume plot
+            st.plotly_chart(fig1)
+            st.plotly_chart(fig2)
+        else:
+            st.write("No yfinance data")
+   
     with rightPane:
         # Display each article with its summary and link
         st.markdown("### Related News")
@@ -178,28 +223,29 @@ def show_news():
             published_at = article.get("publishedAt", "No Date")
             published_at_formatted = pd.to_datetime(published_at).strftime('%B %d, %Y')
             summary = generate_summary(content)
-            st.markdown(f"### <span style='color:gray; font-size:12px;'>{published_at_formatted}</span>", unsafe_allow_html=True)
+            article_text  = article_text  + ' Next article: ' + content  
+            st.markdown(f"### <span style='color:gray; font-size:12px;'>September 22, 2024</span>", unsafe_allow_html=True)
             with st.expander(f"{title}"):
                 st.write(f"{summary}")
                 st.write(f"[Read Full Article]({url})")
-        
+       
         # If fewer than 5 articles are retrieved, search for related news articles
         if len(articles) < 3:
-            
+           
             related_articles = get_related_articles(from_date_str, to_date_str, TRADE_ENTRY)
-        
+       
             # Filter related articles where the trade entry is mentioned in the content
             filtered_related_articles = []
             for article in related_articles:
                 content = article.get("content", "")
                 if article.get("title", "No Title") != '[Removed]':
                     filtered_related_articles.append(article)
-                if len(filtered_related_articles) > 3:
+                if len(filtered_related_articles) > 2:
                     break
-        
+       
             # if len(filtered_related_articles) < 1:
             #     st.markdown(f"### <span style='color:gray; font-size:12px;'>None found</span>", unsafe_allow_html=True)
-        
+       
             # Limit related articles to top 5
             for article in filtered_related_articles[:5]:
                 title = article.get("title", "No Title")
@@ -208,7 +254,22 @@ def show_news():
                 published_at = article.get("publishedAt", "No Date")
                 published_at_formatted = pd.to_datetime(published_at).strftime('%B %d, %Y')
                 summary = generate_summary(description)
-                st.markdown(f"### <span style='color:gray; font-size:12px;'>{published_at_formatted}</span>", unsafe_allow_html=True)
+                article_text  = article_text  + ' Next article: ' + description  
+                st.markdown(f"### <span style='color:gray; font-size:12px;'>September 22, 2024</span>", unsafe_allow_html=True)
                 with st.expander(f"{title}"):
                     st.write(f"{summary}")
                     st.write(f"[Read Full Article]({url})")
+                   
+                   
+                   
+        if article_text  != '':
+            general_summary = total_summary(article_text)
+            st.markdown(
+                        f"""
+                        <div style='background-color: #F2F1F1; padding: 10px; border-radius: 10px; margin-bottom: 10px;'>
+                            <strong>Summary</strong>
+                            <p>{general_summary}</p>
+                            <div style='display: flex; flex-wrap: wrap;'>
+                        """,
+                        unsafe_allow_html=True
+                    )
